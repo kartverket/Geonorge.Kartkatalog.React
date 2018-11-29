481402ced6072929f610e9f953a9481b6fb6d88a
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-29 16:10:34 +0100

    Add component for map container

---
+++ ClientApp/src/components/MapContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import style from './MapContainer.scss';
+
+export class MapContainer extends Component {
+    displayName = MapContainer.name
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+
+  
+    render() {
+      return (
+        <div>
+            Map
+        </div>
+      );
+    }
+  }
+  
