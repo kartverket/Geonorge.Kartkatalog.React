3ecdd318b7da1d3d1fea9b28db548aca38372eb9
Benjamin Dehli <benjamin@arkitektum.no>
2018-12-03 12:43:32 +0100

    Remove unused content

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Download } from './components/Download';
 
 export default class App extends Component {
   displayName = App.name
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   displayName = Layout.name
