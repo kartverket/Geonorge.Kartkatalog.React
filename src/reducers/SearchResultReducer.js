da67274bf5b8205dba653e73a01fb5e4225b033b
Benjamin Dehli <benjamin@arkitektum.no>
2019-01-28 16:10:54 +0100

    Add Translated type name logic to search results reducer

--- src/reducers/SearchResultReducer.js
+++ src/reducers/SearchResultReducer.js
@@ -1,9 +1,10 @@
+import * as Cookies from 'js-cookie';
 import { FETCH_METADATASEARCHRESULTS, FETCH_ARTICLESEARCHRESULTS, FETCH_DROPDOWNSEARCHRESULTS } from '../actions/types';
 
 const initialState = {}
 
-export default function(state = initialState, action) {
-	switch(action.type) {
+export default function (state = initialState, action) {
+	switch (action.type) {
 		case FETCH_METADATASEARCHRESULTS:
 			return {
 				...state,
@@ -15,13 +16,26 @@ export default function(state = initialState, action) {
 				articles: action.payload
 			};
 		case FETCH_DROPDOWNSEARCHRESULTS:
-		return {
-			...state,
-			dropdownResults:{ 
-				...state.dropdownResults,
-				[action.searchResultsType]: action.payload 
+			const getTypeTranslated = () => {
+				if (action.payload.Results && action.payload.Results.length && action.payload.Results[0].TypeTranslated) {
+					return action.payload.Results[0].TypeTranslated;
+				} else if (action.searchResultsType === 'articles') {
+					const selectedLanguage = Cookies.get('_culture') ? Cookies.get('_culture') : 'no';
+					return selectedLanguage === 'en' ? 'Articles' : 'Artikler';
+				} else {
+					return action.searchResultsType
+				}
+			};
+			return {
+				...state,
+				dropdownResults: {
+					...state.dropdownResults,
+					[action.searchResultsType]: {
+						...action.payload,
+						TypeTranslated: getTypeTranslated()
+					}
+				}
 			}
-		}
 		default:
 			return state;
 	}
