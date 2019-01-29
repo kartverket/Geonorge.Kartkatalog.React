9535cb21c8089cdf9221cad44fe0985d65d5fc33
Benjamin Dehli <benjamin@arkitektum.no>
2019-01-29 13:44:59 +0100

    Add classIcon method for search results articles components

--- src/components/partials/SearchResults/ArticleSearchResult.js
+++ src/components/partials/SearchResults/ArticleSearchResult.js
@@ -1,38 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 import { Col, Row } from 'react-bootstrap';
 
 import style from './ArticleSearchResult.scss';
 
 class ArticleSearchResult extends Component {
+	getTypeIcon() {
+		const typeIcons = {
+			StandardPage: ['far', 'square'],
+			NewsPage: ['far', 'check-square']
+		}
+		return typeIcons[this.props.searchResult.Type]
+	}
+
 	render() {
 		return (
 			<Row className={style.listItem}>
-			<Col sm={12}>
-			<span className={style.listItemTitle}>
-			<a href={this.props.searchResult.ShowDetailsUrl}>{this.props.searchResult.Title}</a>
-			<span className={style.listItemType}>
-			{this.props.searchResult.Type}
-			</span>
-			</span>
-			<span className={style.listItemInfo}>
-			{this.props.searchResult.Intro}
-			</span>
-			<span className={style.listItemDate}>
-			{this.props.searchResult.Date}
-			</span>
-			</Col>
+				<Col sm={12}>
+					<span className={style.listItemTitle}>
+						<a href={this.props.searchResult.ShowDetailsUrl}>{this.props.searchResult.Title}</a>
+						<span className={style.listItemType}>
+							{this.props.searchResult.Type}
+							<FontAwesomeIcon icon={this.getTypeIcon()} />
+						</span>
+					</span>
+					<span className={style.listItemInfo}>
+						{this.props.searchResult.Intro}
+					</span>
+					<span className={style.listItemDate}>
+						{this.props.searchResult.Date}
+					</span>
+				</Col>
 			</Row>
-			)
+		)
 	}
 }
 
 ArticleSearchResult.propTypes = {
 	searchResult: PropTypes.object.isRequired
-  }
-  
+}
+
 
 
 export default connect(null)(ArticleSearchResult);
