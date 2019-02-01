6eefbf8cdf98b1af2c7eadcba91528d9ddd3c063
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-01 13:07:24 +0100

    Add tests for facet filter actions

---
+++ src/actions/FacetFilterActions.test.js
@@ -0,0 +1,78 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk'
+
+// Actions to be tested
+import * as facetFilterActions from './FacetFilterActions';
+
+
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares);
+const store = mockStore();
+
+describe('facet_filter_actions', () => {
+    beforeEach(() => { // Runs before each test in the suite
+        store.clearActions();
+    });
+
+    describe('addSelectedFacet', () => {
+        test('Dispatches the correct action and payload', () => {
+            const facet = {
+                Name: 'dataset',
+                Count: 287,
+                NameTranslated: 'Datasett'
+            };
+            const facetField = "type";
+            store.dispatch(facetFilterActions.addSelectedFacet(facet, facetField));
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+
+    describe('removeSelectedFacet', () => {
+        test('Dispatches the correct action and payload', () => {
+            const facet = {
+                Name: 'dataset',
+                Count: 287,
+                NameTranslated: 'Datasett'
+            };
+            const facetField = "type";
+
+            store.dispatch(facetFilterActions.removeSelectedFacet(facet, facetField));
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+
+    describe('updateSelectedFacets', () => {
+        test('Dispatches the correct action and payload', () => {
+            const facets = [
+                {
+                    facets: [
+                        { Name: 'dataset' },
+                        { Name: 'service' }
+                    ],
+                    facetField: 'type'
+                },
+                {
+                    facets: [
+                        { Name: 'Befolkning' },
+                        { Name: 'Energi' }
+                    ],
+                    facetField: 'theme'
+                }
+            ]
+            store.dispatch(facetFilterActions.updateSelectedFacets(facets));
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+
+    describe('updateSelectedFacetsForFacetField', () => {
+        test('Dispatches the correct action and payload', () => {
+            const facets = [
+                { Name: 'dataset' },
+                { Name: 'service' }
+            ];
+            const facetField = 'type';
+            store.dispatch(facetFilterActions.updateSelectedFacetsForFacetField(facets, facetField));
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+});
