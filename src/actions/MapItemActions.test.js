e05e89ebb4dc4712ea9b67bd5da74d9fd267b176
Veronika Andersen <veronika@arkitektum.no>
2019-02-06 11:25:41 +0100

    Create test "Add map item" and "Remove map item"

--- src/actions/MapItemActions.test.js
+++ src/actions/MapItemActions.test.js
@@ -4,10 +4,14 @@ import thunk from 'redux-thunk'
 // Actions to be tested
 import * as mapItemActions from './MapItemActions';
 
-
 const middlewares = [thunk] // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
 const store = mockStore();
+
+const mapItem = {
+    Title: "title mapItem",
+    Uuid: "123456",
+}
 
 describe('map_item_actions', () => {
     beforeEach(() => { // Runs before each test in the suite
@@ -21,5 +25,18 @@ describe('map_item_actions', () => {
         });
     });
 
+    describe('addMapItems', () => {
+        test('Add map item', () => {
+            store.dispatch(mapItemActions.addMapItem(mapItem)); 
+            expect(localStorage.mapItems).toContain("123456");
+        });
+    });
+
+    describe('removeMapItems', () => {
+        test('Remove map item', () => {
+            store.dispatch(mapItemActions.removeMapItem(mapItem)); 
+            expect(localStorage.mapItems).not.toContain("123456");
+        });
+    });
 
 });
