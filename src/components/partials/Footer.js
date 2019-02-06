f8aaf64ea5bb289863a40b2d4740088af46a4b34
Ole <ole@arkitektum.no>
2019-02-06 13:47:21 +0100

    Cleanup of unused code for mapcontainer

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo_dev from '../../images/svg/geonorge-navbar-logo_dev.svg';
+import logo_kart_dev from '../../images/kartverket.png';
 import style from './Footer.scss';
 
 class Footer extends Component {
@@ -9,8 +10,20 @@ class Footer extends Component {
 		return (
 			<div className={style.container}>
 				<div className={style.logo}><img src={logo_dev} alt="logo geonorge" title="Logo for Geonorge" /></div>
-				<div className={style.about}>Om nettstedet</div>
-				<div className={style.contact}>Kontakt info</div>
+				<div className={style.about}>
+					<h3>OM NETTSTEDET</h3>					
+					<p><a href="https://www.geonorge.no/aktuelt/om-geonorge/">Om Geonorge</a></p>
+					<p><a href="http://kartverket.no/Om-Kartverket/Personvern-og-cookies/#_ga=2.93024157.1065069024.1548754965-263923791.1536837570">Personvern og bruk av cookies</a></p>
+				</div>
+				<div className={style.contact}>
+					<h3>KONTAKT</h3>
+					<p>Telefon: 32 11 80 00</p>
+					<p>post@norgedigitalt.no</p>
+					<p>Org. nr.: 971 040 238</p>
+					<hr />
+						En løsning fra
+					<div className={style.logo}><img src={logo_kart_dev} alt="logo geonorge" title="Logo for Kartverket" /></div>
+				</div>
 			</div>
 
 
--- src/components/routes/MapContainer.js
+++ src/components/routes/MapContainer.js
@@ -4,16 +4,14 @@ import { connect } from 'react-redux';
 import { fetchMapItems } from '../../actions/MapItemActions'
 import { MapComponent } from 'r_map';
 import classNames from 'classnames/bind';
-import style from './MapContainer.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 
 class MapContainer extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			activeMenu: false
-		};
+		
 	}
 
 	componentWillMount() {
@@ -23,32 +21,15 @@ class MapContainer extends Component {
 
 	render() {
 
-		let mapContainerClassNames = classNames({
-			[style.map]: true,
-			[style.activeMenu]: this.state.activeMenu
-
-		})
+	
 
 		return (
-			<div className={mapContainerClassNames}>
-				<div className={style.layers}>
-
-
-					<FontAwesomeIcon onClick={() => this.toggleSidebar()} className={style.toggle} icon={this.state.activeMenu ? 'times' : 'bars'} />
-
-					<div className={style.container}><ul>
-						<li>Kartdata 1</li>
-						<li>Kartdata 3</li>
-					</ul>
-					</div>
-				</div>
-				<div className={style.mapContent}><MapComponent services={this.props.mapItems} /> </div>
+			<div>				
+				<MapComponent services={this.props.mapItems} />
 			</div>
 		)
 	}
-	toggleSidebar() {
-		this.setState({ activeMenu: !this.state.activeMenu })
-	}
+	
 }
 
 MapContainer.propTypes = {
