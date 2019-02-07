ff1116951cfd6a9cda9091fa647c8632dceb77af
Veronika Andersen <veronika@arkitektum.no>
2019-02-07 10:06:04 +0100

    Add "DownloadItemReducer"

---
+++ src/reducers/DownloadItemReducer.js
@@ -0,0 +1,13 @@
+import { FETCH_ITEMS_TO_DOWNLOAD } from '../actions/types';
+
+const initialState = []
+
+export default function(state = initialState, action) {
+	switch(action.type) {
+		case FETCH_ITEMS_TO_DOWNLOAD:
+			return action.payload;
+		default:
+			return state;
+	}
+	
+}
