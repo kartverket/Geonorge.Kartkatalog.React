dc01283309247482f6bbc09f5614358f736cb5d0
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-07 13:09:27 +0100

    Update tests and snapshot for main navigation component

--- src/components/partials/MainNavigation.test.js
+++ src/components/partials/MainNavigation.test.js
@@ -9,7 +9,9 @@ import style from './MainNavigation.scss'
 function setup() {
   const props = {
     fetchMapItems: jest.fn(),
-    mapItems: []
+    fetchItemsToDownload: jest.fn(),
+    mapItems: [],
+    itemsToDownload: []
   }
 
   const wrapper = shallow(<MainNavigation {...props} />)
@@ -21,21 +23,26 @@ function setup() {
 }
 
 describe('MainNavigation', () => {
-    it('should render self and subcomponents', () => {
-      const { wrapper } = setup()
-      expect(wrapper.hasClass(style.mainNavigationContainer)).toBe(true);
-      expect(wrapper.find('img').length).toBe(2);
-      expect(wrapper.find(SearchBar).length).toBe(1);
-      expect(wrapper).toMatchSnapshot();
-    })
+  it('should render self and subcomponents', () => {
+    const { wrapper } = setup()
+    expect(wrapper.hasClass(style.mainNavigationContainer)).toBe(true);
+    expect(wrapper.find('img').length).toBe(2);
+    expect(wrapper.find(SearchBar).length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+  })
 
-    it('should include 1 SearchBar component', () => {
-      const { wrapper } = setup()
-      expect(wrapper.find(SearchBar).length).toBe(1);
-    })
+  it('should include 1 SearchBar component', () => {
+    const { wrapper } = setup()
+    expect(wrapper.find(SearchBar).length).toBe(1);
+  })
 
-    it('should call fetchMapItems on mount', () => {
-      const { props } = setup()
-      expect(props.fetchMapItems.mock.calls.length).toBe(1)
-    })
+  it('should call fetchMapItems on mount', () => {
+    const { props } = setup()
+    expect(props.fetchMapItems.mock.calls.length).toBe(1)
+  })
+
+  it('should call fetchItemsToDownload on mount', () => {
+    const { props } = setup()
+    expect(props.fetchItemsToDownload.mock.calls.length).toBe(1)
+  })
 })
