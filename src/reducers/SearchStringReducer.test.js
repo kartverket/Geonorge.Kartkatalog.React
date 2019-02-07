2ff800b8c5e0cc03ef58bf688adeed66d7e6eac9
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-07 16:39:29 +0100

    Add tests and snapshot for search string reducer

---
+++ src/reducers/SearchStringReducer.test.js
@@ -0,0 +1,18 @@
+import reducer from './SearchStringReducer';
+import { UPDATE_SEARCH_STRING } from '../actions/types';
+
+describe('search_string_reducer', () => {
+    it('should return the initial state', () => {
+        const initialState = '';
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should handle UPDATE_SEARCH_STRING', () => {
+        const searchString = "administrative enheter";
+        const updateAction = {
+            type: UPDATE_SEARCH_STRING,
+            payload: searchString
+        };
+        expect(reducer({}, updateAction)).toMatchSnapshot();
+    });
+});
