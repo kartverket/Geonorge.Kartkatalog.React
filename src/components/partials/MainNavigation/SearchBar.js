0e37ae4f05a89ffab4c12a74e7bd5a8b7b072d0b
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-13 11:23:38 +0100

    Add null check and 'no results' message to dropdown search results

--- src/components/partials/MainNavigation/SearchBar.js
+++ src/components/partials/MainNavigation/SearchBar.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchMetadataSearchResults, fetchArticleSearchResults, fetchDropdownSearchResults } from '../../../actions/SearchResultActions';
-import { updateSearchString } from '../../../actions/SearchStringActions';
+import {connect} from 'react-redux';
+import {
+    fetchMetadataSearchResults,
+    fetchArticleSearchResults,
+    fetchDropdownSearchResults
+} from '../../../actions/SearchResultActions';
+import {updateSearchString} from '../../../actions/SearchStringActions';
 
 import SearchResultsTypeList from './SearchResultsTypeList';
 
@@ -11,77 +15,88 @@ import searchIcon from '../../../images/svg/search-icon.svg';
 
 class SearchBar extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchString: '',
-			showResults: false
-		};
-		this.onFocus = this.onFocus.bind(this);
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-		this.showResults = this.showResults.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchString: '',
+            showResults: false
+        };
+        this.onFocus = this.onFocus.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.showResults = this.showResults.bind(this);
+    }
 
-	hideDropdownResults() {
-		this.setState({
-			showResults: false
-		});
-	}
+    hideDropdownResults() {
+        this.setState({
+            showResults: false
+        });
+    }
 
-	showResults() {
-		this.setState({
-			showResults: true
-		});
-	}
+    showResults() {
+        this.setState({
+            showResults: true
+        });
+    }
 
-	handleClick = (e) => {
-		if (this.node && this.node.contains(e.target)) {
-			return;
-		}
+    handleClick = (e) => {
+        if (this.node && this.node.contains(e.target)) {
+            return;
+        }
 
-		this.hideDropdownResults();
-	}
+        this.hideDropdownResults();
+    }
 
-	onChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value
-		})
-		this.props.fetchDropdownSearchResults(e.target.value);
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        this.props.fetchDropdownSearchResults(e.target.value);
 
-	}
+    }
 
-	onFocus(e) {
-		this.showResults();
-	}
+    onFocus(e) {
+        this.showResults();
+    }
 
-	onSubmit(e) {
-		e.preventDefault();
-		this.hideDropdownResults();
-		this.props.fetchMetadataSearchResults(this.state.searchString);
-		this.props.fetchArticleSearchResults(this.state.searchString);
-		this.props.updateSearchString(this.state.searchString);
-	}
+    onSubmit(e) {
+        e.preventDefault();
+        this.hideDropdownResults();
+        this.props.fetchMetadataSearchResults(this.state.searchString);
+        this.props.fetchArticleSearchResults(this.state.searchString);
+        this.props.updateSearchString(this.state.searchString);
+    }
 
-	componentWillMount() {
-		document.addEventListener('mousedown', this.handleClick, false);
-	}
+    componentWillMount() {
+        document.addEventListener('mousedown', this.handleClick, false);
+    }
 
-	componentWillUnMount() {
-		document.addEventListener('mousedown', this.handleClick, false);
-	}
+    componentWillUnMount() {
+        document.addEventListener('mousedown', this.handleClick, false);
+    }
 
-	renderDropdownResults() {
-		if (this.props.dropdownResults) {
-			const resultsTypeElements = Object.keys(this.props.dropdownResults).filter(searchResultsType => {
-				return this.props.dropdownResults[searchResultsType].NumFound;
-			}).map((searchResultsType, i) => {
-				let searchResults = this.props.dropdownResults[searchResultsType];
-				return <SearchResultsTypeList onShowResults={() => this.hideDropdownResults()} searchString={this.state.searchString} searchResults={searchResults} searchResultsType={searchResultsType} key={i} />
-			})
-			return resultsTypeElements;
-		}
-	}
+
+    renderDropdownResults() {
+
+        if (this.props.dropdownResults && Object.keys(this.props.dropdownResults).length) {
+            let hasResults = false;
+            let resultsTypeElements = Object.keys(this.props.dropdownResults).filter(searchResultsType => {
+                return this.props.dropdownResults[searchResultsType].NumFound;
+            }).map((searchResultsType, i) => {
+                let searchResults = this.props.dropdownResults[searchResultsType];
+                if (searchResults) {
+                    hasResults = true;
+                }
+                return <SearchResultsTypeList onShowResults={() => this.hideDropdownResults()}
+                                              searchString={this.state.searchString} searchResults={searchResults}
+                                              searchResultsType={searchResultsType} key={i}/>
+            });
+            if (!hasResults) {
+                resultsTypeElements = <div>Ingen treff for {this.state.searchString}</div>;
+            }
+            return resultsTypeElements;
+        }
+    }
 
 	render() {
 		return (
@@ -99,21 +114,21 @@ class SearchBar extends Component {
 }
 
 SearchBar.propTypes = {
-	fetchMetadataSearchResults: PropTypes.func.isRequired,
-	fetchArticleSearchResults: PropTypes.func.isRequired,
-	fetchDropdownSearchResults: PropTypes.func.isRequired,
-	dropdownResults: PropTypes.object.isRequired
-}
+    fetchMetadataSearchResults: PropTypes.func.isRequired,
+    fetchArticleSearchResults: PropTypes.func.isRequired,
+    fetchDropdownSearchResults: PropTypes.func.isRequired,
+    dropdownResults: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-	dropdownResults: state.searchResults.dropdownResults ? state.searchResults.dropdownResults : {}
+    dropdownResults: state.searchResults.dropdownResults ? state.searchResults.dropdownResults : {}
 });
 
 const mapDispatchToProps = {
-	fetchMetadataSearchResults,
-	fetchArticleSearchResults,
-	fetchDropdownSearchResults,
-	updateSearchString
+    fetchMetadataSearchResults,
+    fetchArticleSearchResults,
+    fetchDropdownSearchResults,
+    updateSearchString
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
