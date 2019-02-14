4c0c03f75d554fa4bc72f7076ce1423df19398b1
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-14 11:30:48 +0100

    Add tests for metadata search result actions

--- src/actions/SearchResultActions.test.js
+++ src/actions/SearchResultActions.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 
 // Actions to be tested
 import * as searchResultActions from './SearchResultActions';
+import {FETCH_AVAILABLEFACETS} from "./types";
 
 const middlewares = [thunk] // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
@@ -15,37 +16,39 @@ describe('search_result_actions', () => {
     });
 
     describe('fetchMetadataSearchResults', () => {
-        test('Get search results from facets', async () => {
-            const facets = {
-                type: [
-                    { Name: 'dataset' },
-                ],
-                dataaccess: [
-                    { Name: "Open data" }
-                ]
-            }
-            await store.dispatch(searchResultActions.fetchMetadataSearchResults(null, facets)); // searchString = "", facets = null, Offset = 1, append = false
-            expect(store.getActions()).toMatchSnapshot();
+        test('Set correct default value for "searchString"', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults()); // searchString = "", facets = null, Offset = 1, append = false
+            expect(store.getActions()[0].searchString).toBe("");
         });
 
-        test('Get search results from search string', async () => {
-            const searchString = 'administrative enheter';
-            await store.dispatch(searchResultActions.fetchMetadataSearchResults(searchString)); // searchString = "", facets = null, Offset = 1, append = false
-            expect(store.getActions()).toMatchSnapshot();
+        test('Get correct payload from API', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults());
+            expect(store.getActions()[0].payload).not.toBeNull();
+            expect(store.getActions()[0].payload).toHaveProperty('Facets');
+            expect(store.getActions()[0].payload).toHaveProperty('Limit');
+            expect(store.getActions()[0].payload).toHaveProperty('Offset');
+            expect(store.getActions()[0].payload).toHaveProperty('NumFound');
         });
 
-        test('Get search results from search string and facets', async () => {
-            const searchString = 'administrative enheter';
-            const facets = {
-                type: [
-                    { Name: 'dataset' },
-                ],
-                dataaccess: [
-                    { Name: "Open data" }
-                ]
-            }
-            await store.dispatch(searchResultActions.fetchMetadataSearchResults(searchString, facets)); // searchString = "", facets = null, Offset = 1, append = false
-            expect(store.getActions()).toMatchSnapshot();
+        test('Get search results in payload from API', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults());
+            expect(store.getActions()[0].payload.Results.length).toBeGreaterThan(0);
+            expect(store.getActions()[0].payload.NumFound).toBeGreaterThan(0);
+        });
+
+        test('Dispatch correct type as default', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults());
+            expect(store.getActions()[0].type).toBe("FETCH_METADATASEARCHRESULTS");
+        });
+
+        test('Dispatch correct type if "append" is true', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults(undefined, null, 1, true));
+            expect(store.getActions()[0].type).toBe("APPEND_TO_METADATASEARCHRESULTS");
+        });
+
+        test('Dispatch "FETCH_AVAILABLEFACETS"', async () => {
+            await store.dispatch(searchResultActions.fetchMetadataSearchResults(undefined, null, 1, true));
+            expect(store.getActions()[1].type).toBe("FETCH_AVAILABLEFACETS");
         });
     });
 });
