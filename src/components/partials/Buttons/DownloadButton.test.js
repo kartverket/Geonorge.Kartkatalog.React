ca173930ae4ba910aba38908793ae3b2e91b1ee6
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-15 16:14:13 +0100

    Update test for download button

--- src/components/partials/Buttons/DownloadButton.test.js
+++ src/components/partials/Buttons/DownloadButton.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { shallow } from "enzyme";
-import { DownloadButton } from './DownloadButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {shallow} from "enzyme";
+import {DownloadButton} from './DownloadButton';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function setupSearchResultDownloadButton() {
     const searchResult = {
         DistributionProtocol: "GEONORGE:DOWNLOAD"
-    }
+    };
     const props = {
         searchResult: searchResult,
         addItemSelectedForDownload: jest.fn(),
         removeItemSelectedForDownload: jest.fn()
-    }
-    const wrapper = shallow(<DownloadButton {...props} />)
+    };
+    const wrapper = shallow(<DownloadButton {...props} />);
 
     return {
         props,
@@ -25,14 +25,13 @@ function setupSearchResultDownloadLink() {
         DistributionUrl: "test.no",
         DistributionProtocol: "WWW:DOWNLOAD-1.0-http--download",
         Type: "dataset"
-
-    }
+    };
     const props = {
         searchResult: searchResult,
         addItemSelectedForDownload: jest.fn(),
         removeItemSelectedForDownload: jest.fn()
-    }
-    const wrapper = shallow(<DownloadButton {...props} />)
+    };
+    const wrapper = shallow(<DownloadButton {...props} />);
 
     return {
         props,
@@ -41,38 +40,38 @@ function setupSearchResultDownloadLink() {
 }
 
 describe('DownloadButton', () => {
-    
+
     it('should render self', () => {
-        const { wrapper } = setupSearchResultDownloadButton()
-        expect( wrapper).toMatchSnapshot();
-    })
+        const {wrapper} = setupSearchResultDownloadButton();
+        expect(wrapper).toMatchSnapshot();
+    });
 
     it('Show download button when item is selected for download', () => {
-        let { wrapper } = setupSearchResultDownloadButton()
-        wrapper = wrapper.setState({isSelectedForDownload: true})
+        let {wrapper} = setupSearchResultDownloadButton();
+        wrapper = wrapper.setState({isSelectedForDownload: true});
 
-        expect(wrapper.prop("className")).toContain("off")
-        expect(wrapper.prop("onClick").toString()).toContain("removeFromDownloadList(button)")
-        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('trash')        
-        expect(wrapper.find("span").first().html()).toContain('Fjern nedlasting')
-    })
+        expect(wrapper.prop("className")).toContain("off");
+        expect(wrapper.prop("onClick").toString()).toContain("removeFromDownloadList(button)");
+        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('trash');
+        expect(wrapper.find("span").first().html()).toContain('Fjern nedlasting');
+    });
 
     it('Show download button when item is not selected for download', () => {
-        let { wrapper } = setupSearchResultDownloadButton()
-        wrapper = wrapper.setState({isSelectedForDownload: false})
+        let {wrapper} = setupSearchResultDownloadButton();
+        wrapper = wrapper.setState({isSelectedForDownload: false});
 
-        expect(wrapper.prop("className")).toContain("on")
-        expect(wrapper.prop("onClick").toString()).toContain("addToDownloadList(button)")
-        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('cloud-download')        
-        expect(wrapper.find("span").first().html()).toContain('Last ned')
-    })
+        expect(wrapper.prop("className")).toContain("on");
+        expect(wrapper.prop("onClick").toString()).toContain("addToDownloadList(button)");
+        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('cloud-download');
+        expect(wrapper.find("span").first().html()).toContain('Last ned');
+    });
 
     it('Show download link', () => {
-        let { wrapper } = setupSearchResultDownloadLink()
+        let {wrapper} = setupSearchResultDownloadLink();
 
-        expect(wrapper.prop("className")).toContain("on")
-        expect(wrapper.prop("href")).toBe('test.no')
-        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('download')        
-        expect(wrapper.find("span").first().html()).toContain('Til nedlasting')
-    })
-})
+        expect(wrapper.prop("className")).toContain("on");
+        expect(wrapper.prop("href")).toBe('test.no');
+        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('external-link-square');
+        expect(wrapper.find("span").first().html()).toContain('Til nedlasting');
+    });
+});
