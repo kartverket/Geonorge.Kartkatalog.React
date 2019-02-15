1f0d2945d701bc025036d65f8606b3b6bf3995b6
Veronika Andersen <veronika@arkitektum.no>
2019-02-15 12:47:33 +0100

    Only show one map button on dataset

--- src/components/partials/Buttons/MapButton.js
+++ src/components/partials/Buttons/MapButton.js
@@ -48,12 +48,6 @@ export class MapButton extends Component {
             addLayers: []
         }
     }
-    getMapItems() {
-        let mapItems = this.props.searchResult.DatasetServicesWithShowMapLink.map((item, i) => {
-            return this.getMapItem(item);
-        });
-        return mapItems;
-    }
     addToMap(mapItem) {
         this.props.addMapItem(mapItem);
         this.setState({
@@ -75,11 +69,12 @@ export class MapButton extends Component {
         });
     }
 
-    renderButton(mapItem) {
+    renderButton() {
+        let mapItem = this.props.searchResult.Type === "dataset" ? this.getMapItem(this.props.searchResult.DatasetServicesWithShowMapLink[0]) : this.getMapItem()
         let isAdded = this.mapItemIsAddedToLocalStorage(mapItem);
         let action = isAdded
             ? () => this.removeFromMap([mapItem])
-            : () => this.addToMap([mapItem]);        
+            : () => this.addToMap([mapItem]);
         let icon = <FontAwesomeIcon icon={isAdded ? ['far', 'map-marker-minus'] : ['far', 'map-marker-plus']} key="icon" />
         let buttonClass = isAdded ? 'off' : 'on';
         let textContent = React.createElement('span', { key: "textContent" }, isAdded ? 'Fjern fra kart' : 'Legg til i kart')
@@ -88,54 +83,10 @@ export class MapButton extends Component {
         return React.createElement('span', { onClick: action, className: buttonClass }, childElements);
     }
 
-    renderButtonSelectAll() {
-        let mapItems = this.getMapItems();
-        let isAdded = this.datasetServicesIsAddedToLocalStorage();
-        let action = isAdded
-            ? () => this.removeFromMap(mapItems)
-            : () => this.addToMap(mapItems);
-        let icon = <FontAwesomeIcon icon={isAdded ? ['far', 'map-marker-minus'] : ['far', 'map-marker-plus']} key="icon" />
-        let buttonClass = isAdded ? 'off' : 'on';
-        let textContent = React.createElement('span', { key: "textContent" }, this.isAdded ? 'Fjern fra kart' : 'Legg til i kart')
-
-        let childElements = [icon, textContent];
-        return React.createElement('span', { onClick: action, className: buttonClass }, childElements);
-    }
-
-    renderButtonExpand() {
-        return <FontAwesomeIcon onClick={() => this.toggleExpand()} className={style.expandmapbutton} icon={this.state.expanded ? 'angle-up' : 'angle-down'} />
-    }
-
-    renderButtonList() {
-        let mapItems = this.props.searchResult.DatasetServicesWithShowMapLink.map((item, i) => {
-            let mapItem = this.getMapItem(item);
-            return (
-                <span key={i} className="">
-                    <a>{mapItem.Title}</a>
-                    {this.renderButton(mapItem)}
-                </span>
-            )
-        });
-        return React.createElement('span', {}, mapItems);
-    }
-
     render() {
         if (this.props.searchResult.ShowMapLink) {
-            if (this.props.searchResult.Type === "dataset") {
-                return (
-                    <span>
-                        {this.renderButtonExpand()}
-                        <span className={this.state.expanded ? 'sublist open' : 'sublist'}>
-                            {this.renderButtonSelectAll()}
-                            {this.renderButtonList()}
-                        </span>
-                    </span>
-                )
-            }
-            else {
-                let mapItem = this.getMapItem()
-                return this.renderButton(mapItem);
-            }
+            let mapItem = this.getMapItem()
+            return this.renderButton(mapItem);
         }
         return null;
     }
--- src/components/partials/Buttons/MapButton.test.js
+++ src/components/partials/Buttons/MapButton.test.js
@@ -4,14 +4,55 @@ import { MapButton } from './MapButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { wrap } from 'module';
 
-function setupSearchResultWithShowMapLinkTrue() {
+function setupServiceWithShowMapLinkTrue() {
     const searchResult = {
         ShowMapLink: true,
         Uuid: "1234",
         Title: "Test title",
         DistributionProtocol: "test protocol",
         GetCapabilitiesUrl: "test url",
-        addLayers: []
+        addLayers: [],
+        Type: "service"
+    }
+    const props = {
+        searchResult: searchResult,
+        addMapItem: jest.fn(),
+        removeMapItem: jest.fn()
+    }
+    const wrapper = shallow(<MapButton {...props} />)
+
+    return {
+        props,
+        wrapper
+    }
+}
+
+function setupDatasetWithShowMapLinkTrue() {
+    const searchResult = {
+        ShowMapLink: true,
+        Uuid: "1234",
+        Title: "Test title",
+        DistributionProtocol: "test protocol",
+        GetCapabilitiesUrl: "test url",
+        addLayers: [],
+        Type: "dataset",
+        DatasetServicesWithShowMapLink: [
+            {
+                Uuid: "1111",
+                Title: "Test title",
+                DistributionProtocol: "test protocol",
+                GetCapabilitiesUrl: "test url",
+                addLayers: []
+            },
+            {
+                Uuid: "2222",
+                Title: "Test title",
+                DistributionProtocol: "test protocol",
+                GetCapabilitiesUrl: "test url",
+                addLayers: []
+            }
+        ]
+
     }
     const props = {
         searchResult: searchResult,
@@ -41,15 +82,17 @@ function setupSearchResultWithShowMapLinkFalse() {
     }
 }
 
+
+
 describe('MapButton', () => {
 
     it('should render self', () => {
-        const { wrapper } = setupSearchResultWithShowMapLinkTrue()
+        const { wrapper } = setupServiceWithShowMapLinkTrue()
         expect(wrapper).toMatchSnapshot();
     })
 
-    it('Searchresult with ShowMapLink true and isAdded false', () => {
-        let { wrapper } = setupSearchResultWithShowMapLinkTrue()
+    it('Service with ShowMapLink true and isAdded false', () => {
+        let { wrapper } = setupServiceWithShowMapLinkTrue()
 
         expect(wrapper.prop("className")).toContain("on")
         expect(wrapper.prop("onClick").toString()).toContain("addToMap([mapItem])")
@@ -57,8 +100,17 @@ describe('MapButton', () => {
         expect(wrapper.find("span").first().html()).toContain('Legg til i kart')
     })
 
-    it('Searchresult with ShowMapLink true and isAdded true', () => {
-        let { wrapper } = setupSearchResultWithShowMapLinkTrue()
+    it('Dataset with ShowMapLink true and isAdded false', () => {
+        let { wrapper } = setupDatasetWithShowMapLinkTrue()
+
+        expect(wrapper.prop("className")).toContain("on")
+        expect(wrapper.prop("onClick").toString()).toContain("addToMap([mapItem])")
+        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('map-marker-plus')
+        expect(wrapper.find("span").first().html()).toContain('Legg til i kart')
+    })
+
+    it('Service with ShowMapLink true and isAdded true', () => {
+        let { wrapper } = setupServiceWithShowMapLinkTrue()
         wrapper.setState({
             selectedMapItems: [{
                 Uuid: "1234",
@@ -75,7 +127,26 @@ describe('MapButton', () => {
         expect(wrapper.find("span").first().html()).toContain('Fjern fra kart')
     })
 
-    it('Searchresult with ShowMapLink false', () => {
+    it('Service with ShowMapLink true and isAdded true', () => {
+        let { wrapper } = setupDatasetWithShowMapLinkTrue()
+        wrapper.setState({
+            selectedMapItems: [{
+                Uuid: "1234",
+                Title: "Test title",
+                DistributionProtocol: "test protocol",
+                GetCapabilitiesUrl: "test url",
+                addLayers: Array(0)
+            }]
+        })
+
+        expect(wrapper.prop("className")).toContain("off")
+        expect(wrapper.prop("onClick").toString()).toContain("removeFromMap([mapItem])")
+        expect(wrapper.find(FontAwesomeIcon).first().prop("icon")).toContain('map-marker-minus')
+        expect(wrapper.find("span").first().html()).toContain('Fjern fra kart')
+    })
+
+
+    it('Service with ShowMapLink false', () => {
         let { wrapper } = setupSearchResultWithShowMapLinkFalse()
 
         expect(wrapper.hasClass()).toBe(false)
