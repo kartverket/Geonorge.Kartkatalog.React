35e7d4526ed25ce24f78288b4569e34e8b2f6bef
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-15 13:24:18 +0100

    Don't trigger new search results fetch when returning to /home

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
 
-import { Route, Switch } from 'react-router'
-import { ConnectedRouter } from 'connected-react-router'
-import configureStore, { history } from './utils/configureStore'
+import {Route, Switch} from 'react-router'
+import {ConnectedRouter} from 'connected-react-router'
+import configureStore, {history} from './utils/configureStore'
+
 
 import Home from './components/routes/Home';
 import Footer from './components/partials/Footer';
 import MapContainer from './components/routes/MapContainer';
+import Metadata from "./components/routes/Metadata";
 
 import MainNavigation from './components/partials/MainNavigation';
 import style from './App.scss';
 
 import './scss/styles.scss';
-
+import {fetchArticleSearchResults, fetchMetadataSearchResults} from "./actions/SearchResultActions";
 
 const initialState = {};
 const store = configureStore(initialState);
 
-
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div className="kartkatalogen">
-            <MainNavigation />
-            <div className={style.pageContent}>
-            <div className={style.container}>
-              <Switch>
+    componentWillMount() {
+        store.dispatch(fetchMetadataSearchResults());
+        store.dispatch(fetchArticleSearchResults());
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <div className="kartkatalogen">
+                        <MainNavigation/>
+                        <div className={style.pageContent}>
+                            <div className={style.container}>
+                                <Switch>
                 <Route exact path="/" render={() => (<Home />)} />
                 <Route exact path="/kart" render={() => (<MapContainer />)} />
                 <Route render={() => (<div>Miss</div>)} />
-              </Switch>
-              </div>
-              <footer className={style.footer}>
-                  <Route exact path="/" render={()=> (<Footer />)} />
-              </footer>
-            </div>            
-          </div>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
+                                    <Route render={() => (<div>Miss</div>)}/>
+                                </Switch>
+                            </div>
+                            <footer className={style.footer}>
+                                <Route exact path="/" render={() => (<Footer/>)}/>
+                            </footer>
+                        </div>
+                    </div>
+                </ConnectedRouter>
+            </Provider>
+        );
+    }
 }
 
 export default App;
