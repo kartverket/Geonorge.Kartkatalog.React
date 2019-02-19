43c866a6ef671433b73495886c3831a31cb8deed
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-19 14:51:40 +0100

    Add component for distributions list

---
+++ src/components/routes/Metadata/DistributionsList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import MetadataSearchResult from "../../partials/SearchResults/MetadataSearchResult";
+import style from "./DistributionsList.scss";
+
+class DistributionsList extends Component {
+    render() {
+        const distributions = this.props.distributions.map((distribution, i) => {
+            return <MetadataSearchResult searchResult={distribution} key={i}/>;
+        });
+        return (
+            <div className={style.distributionsList}>{distributions}</div>
+        );
+    }
+}
+
+DistributionsList.propTypes = {
+    distributions: PropTypes.array.isRequired
+};
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(DistributionsList);
