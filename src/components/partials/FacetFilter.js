3c85c371eb98f382a519b2c47f5cc86e4b145466
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-19 15:49:34 +0100

    Adjust logic for selected facets label

--- src/components/partials/FacetFilter.js
+++ src/components/partials/FacetFilter.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import classNames from 'classnames/bind';
 import Facet from './FacetFilter/Facet';
 
 import FacetFilterItem from './FacetFilter/FacetFilterItem';
-import { ErrorBoundary } from '../../components/ErrorBoundary'
+import {ErrorBoundary} from '../../components/ErrorBoundary'
 
 
 import style from './FacetFilter.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 class FacetFilter extends Component {
     constructor(props) {
@@ -23,53 +23,67 @@ class FacetFilter extends Component {
         return this.props.availableFacets ? Object.keys(this.props.availableFacets) : [];
     }
 
-    getSelectedFacetItems() {        
+    getSelectedFacetItems() {
         return this.props.selectedFacets ? Object.keys(this.props.selectedFacets) : [];
     }
+
     toggleFacets() {
         this.setState({
-            expanded : !this.state.expanded
+            expanded: !this.state.expanded
         })
     }
 
     renderSelectedFacets() {
-        let selectedFacets = this.getSelectedFacetItems();         
+        let selectedFacets = this.getSelectedFacetItems();
         let selectedList = selectedFacets.map(facetField => {
-            return this.props.selectedFacets[facetField].map((facet, i) => {                
+            return this.props.selectedFacets[facetField].map((facet, i) => {
                 return <Facet removable={'true'} facet={facet} facetField={facetField} key={i}/>;
             });
-            
-        });        
-        return  <div>{selectedList}</div>
+
+        });
+        return <div>{selectedList}</div>
     }
-    showlabel() {        
-      return false;
+
+    hasSelectedFacets() {
+        let hasSelectedFacets = false;
+        Object.keys(this.props.selectedFacets).forEach(facetField => {
+                if (this.props.selectedFacets[facetField].length) {
+                    hasSelectedFacets = true;
+                    return false;
+                }
+            }
+        );
+        return hasSelectedFacets;
     }
+
     renderFacets() {
         const togglefacetClassnames = classNames({
-            [style.facetFilter] : true,
-            open : this.state.expanded
-        })
+            [style.facetFilter]: true,
+            open: this.state.expanded
+        });
 
-        
-        let availableFacets = this.getFacetFilterItems();                
+
+        let availableFacets = this.getFacetFilterItems();
         let facets = availableFacets.map((facetField, i) => {
-            return <ErrorBoundary key={facetField} ><FacetFilterItem facetFilterItem={this.props.availableFacets[facetField]} key={facetField} /></ErrorBoundary>;
+            return <ErrorBoundary key={facetField}><FacetFilterItem
+                facetFilterItem={this.props.availableFacets[facetField]} key={facetField}/></ErrorBoundary>;
         });
         return (
-        <div className={togglefacetClassnames}>
-            <label className={this.showlabel() ? style.hideLabel : style.showLabel}>Valgte filter: </label>
-                {this.renderSelectedFacets()}           
-            <ul>
-                {facets}
-            </ul>
-        </div>);
-    }    
+            <div className={togglefacetClassnames}>
+                <label className={this.hasSelectedFacets() ? style.hideLabel : style.showLabel}>Valgte filter: </label>
+                {this.renderSelectedFacets()}
+                <ul>
+                    {facets}
+                </ul>
+            </div>);
+    }
+
     render() {
         return (
             <div>
-                <div className={style.togglefacet} onClick={() => this.toggleFacets()}><FontAwesomeIcon icon={this.state.expanded ? ['fas','times'] : ['fas','bars']} /></div>                                                                    
-                {this.renderFacets()}                
+                <div className={style.togglefacet} onClick={() => this.toggleFacets()}><FontAwesomeIcon
+                    icon={this.state.expanded ? ['fas', 'times'] : ['fas', 'bars']}/></div>
+                {this.renderFacets()}
             </div>
         )
     }
@@ -78,7 +92,7 @@ class FacetFilter extends Component {
 // Validering av props... 
 FacetFilter.propTypes = {
     searchResults: PropTypes.object
-}
+};
 
 // Metode som binder state til lokale props
 const mapStateToProps = state => ({
