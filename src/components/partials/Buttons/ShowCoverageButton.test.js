da19b0aa3650b7462d08e90ee816567f78ccc4be
Veronika Andersen <veronika@arkitektum.no>
2019-02-28 15:17:07 +0100

    Delete empty coverage button test file

--- src/components/partials/Buttons/ShowCoverageButton.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { shallow } from "enzyme";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ShowCoverageButton } from './ShowCoverageButton';
-import style from './Buttons.scss'
-
