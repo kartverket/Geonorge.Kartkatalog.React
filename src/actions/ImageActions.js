4271a89f6bf9f58bb359d07f63f72a778d81bea8
Benjamin Dehli <benjamin@arkitektum.no>
2019-03-04 09:55:59 +0100

    Add action for images

---
+++ src/actions/ImageActions.js
@@ -0,0 +1,17 @@
+import logoDev from "../images/svg/geonorge-navbar-logo_dev.svg";
+import logoTest from "../images/svg/geonorge-navbar-logo_test.svg";
+import logo from "../images/svg/geonorge-navbar-logo.svg";
+
+export const getGeonorgeLogo = () => () => {
+    const imageSrc = () => {
+        switch (process.env.REACT_APP_ENVIRONMENT) {
+            case 'dev':
+                return logoDev;
+            case 'test':
+                return logoTest;
+            default:
+                return logo;
+        }
+    };
+    return imageSrc();
+};
