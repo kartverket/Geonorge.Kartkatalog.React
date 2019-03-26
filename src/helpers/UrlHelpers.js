11077af6b2a76ab6cc5cf93647edfb15ffc5cf00
Benjamin Dehli <benjamin@arkitektum.no>
2019-03-26 11:24:23 +0100

    Add logic for mapping previously used URL parameter structure

---
+++ src/helpers/UrlHelpers.js
@@ -0,0 +1,38 @@
+export const convertUrl = (url) => {
+    var convertedUrl = '';
+    url = url.substring(url.indexOf("?")); // Removes everything before first parameter
+
+    var urlParams = new URLSearchParams(url);
+    var urlParamsGrouped = {};
+
+    const facetOldUrlTypeRegex = /Facets\[[0-9]\]/;
+    const isOldUrlType = facetOldUrlTypeRegex.exec(url) && facetOldUrlTypeRegex.exec(url).length ? true : false;
+
+    for (let pair of urlParams.entries()) {
+        // Get search string parameters
+        if (pair[0] === 'text') {
+            const paramGroup = 'searchString';
+            urlParamsGrouped[paramGroup] = {
+                name: pair[0],
+                value: pair[1]
+            };
+        } else {
+            const facetRegex = /Facets\[[0-9]\]/;
+            if (facetRegex.exec(pair[0]) && facetRegex.exec(pair[0]).length) { // Get facet parameters
+                const paramGroup = facetRegex.exec(pair[0])[0];
+                const paramPropName = pair[0].replace(paramGroup + '.', '');
+                if (!urlParamsGrouped[paramGroup]) {
+                    urlParamsGrouped[paramGroup] = {};
+                }
+                urlParamsGrouped[paramGroup][paramPropName] = pair[1];
+            }
+        }
+    }
+
+    Object.keys(urlParamsGrouped).forEach(urlParamsGroupName => {
+        convertedUrl += convertedUrl && convertedUrl !== '' ? '&' : '?';
+        convertedUrl += `${urlParamsGrouped[urlParamsGroupName].name}=${urlParamsGrouped[urlParamsGroupName].value}`;
+    });
+
+    return isOldUrlType ? convertedUrl : url;
+}
--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import WebFont from 'webfontloader';
+import { convertUrl } from './helpers/UrlHelpers'
 import './layout/icons';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 
-WebFont.load({
-    google: {
-        families: ['Raleway:100,400,500,700', 'Open Sans:400,600,700', 'sans-serif']
-    }
-});
+if (window.location.search !== convertUrl(window.location.search)) {
+    window.location.href = window.location.origin + convertUrl(window.location.search);
+} else {
+    WebFont.load({
+        google: {
+            families: ['Raleway:100,400,500,700', 'Open Sans:400,600,700', 'sans-serif']
+        }
+    });
+    ReactDOM.render(<App />, document.getElementById('root'));
+    serviceWorker.unregister();
+}
 
-ReactDOM.render(<App/>, document.getElementById('root'));
 
-serviceWorker.unregister();
+
