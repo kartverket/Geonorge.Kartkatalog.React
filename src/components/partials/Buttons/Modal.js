104dc21e61fa84f2ec5227e6c43de97fda6e3f78
Benjamin Dehli <benjamin@arkitektum.no>
2019-05-03 14:40:01 +0200

    Adjust modal component

--- src/components/partials/Buttons/Modal.js
+++ src/components/partials/Buttons/Modal.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
+
 import style from './Modal.scss';
 import styleBtn from './Buttons.scss';
 
-export const Modal = ({ handleClose, show, children }) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
-  
+
+class Modal extends Component {
+
+  handleClick = (e) => {
+    if (this.node && this.node.contains(e.target)) {
+      return;
+    }
+    this.props.handleClose();
+  }
+
+  escFunction = (event) => {
+    if (event.keyCode === 27) {
+      this.props.handleClose();
+    }
+  }
+
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+    document.addEventListener("keydown", this.escFunction, false);
+
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+    document.removeEventListener("keydown", this.escFunction, false);
+
+  }
+
+  render() {
+    const modalClassnames = classNames({
+      [style.modal]: true,
+      [style.hidden]: !this.props.show
+    });
+    const modalButtonClassnames = classNames({
+      [styleBtn.btn]: true,
+      [style.modalButton]: true
+    });
     return (
-      <div className={showHideClassName}>
-        <section className="modal-main">
-          {children}
-          <button className={styleBtn.btn} onClick={handleClose}>Lukk</button>
+      <div className={modalClassnames}>
+        <section className={style.modalMain}>
+          {this.props.children}
+          <button className={modalButtonClassnames} onClick={this.props.handleClose}>Lukk</button>
         </section>
       </div>
     );
-  };
+  }
+}
+
+export default connect(null, null)(Modal);
--- src/components/partials/Buttons/ShowCoverageButton.js
+++ src/components/partials/Buttons/ShowCoverageButton.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import style from './Buttons.scss';
-import {Modal} from './Modal.js'
+import Modal from './Modal.js'
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
