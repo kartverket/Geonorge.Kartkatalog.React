86da52bf0f4451e033e73389b2c10e5634e564ae
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-21 13:52:45 +0200

    Use language resources for help button

--- src/components/partials/Buttons/HelpButton.js
+++ src/components/partials/Buttons/HelpButton.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import style from './Buttons.scss';
 
+import { getResource } from '../../../helpers/ResourceHelpers'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export class HelpButton extends Component {
@@ -12,20 +14,20 @@ export class HelpButton extends Component {
     }
 
     render() {
-
+        let buttonDescription = getResource(this.props.resources, 'Help', 'Hjelp');
         // TODO styling
         if (this.props.metadata.HelpUrl) {
             let url = this.props.metadata.HelpUrl
-            let icon = <FontAwesomeIcon title="Hjelp" icon={['far', 'question-circle']} key="icon" />;
+            let icon = <FontAwesomeIcon title={buttonDescription} icon={['far', 'question-circle']} key="icon" />;
             let buttonClass = style.btn;
-            let textContent = React.createElement('span', { key: "textContent" }, 'Hjelp');
+            let textContent = React.createElement('span', { key: "textContent" }, buttonDescription);
 
             let childElements = [icon, textContent];
             return React.createElement('a', { href: url, className: buttonClass }, childElements);
         } else {
-            let icon = <FontAwesomeIcon title="Hjelp" icon={['far', 'question-circle']} key="icon" />
+            let icon = <FontAwesomeIcon title={buttonDescription} icon={['far', 'question-circle']} key="icon" />
             let buttonClass = style.btn + ' disabled'
-            let textContent = React.createElement('span', { key: "textContent" }, 'Hjelp');
+            let textContent = React.createElement('span', { key: "textContent" }, buttonDescription);
             let childElements = [icon, textContent];
             return React.createElement('span', { className: buttonClass }, childElements);
         }
@@ -37,4 +39,8 @@ HelpButton.propTypes = {
     metadata: PropTypes.object.isRequired
 };
 
-export default connect(null, null)(HelpButton);
+const mapStateToProps = state => ({
+    resources: state.resources
+});
+
+export default connect(mapStateToProps, null)(HelpButton);
