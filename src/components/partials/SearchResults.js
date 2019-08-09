ab93c09ddd61fc81b950cdd45890b55088e20f04
Benjamin Dehli <benjamin@arkitektum.no>
2019-08-09 13:03:35 +0200

    Add language resources to search results component

--- src/components/partials/SearchResults.js
+++ src/components/partials/SearchResults.js
@@ -1,17 +1,24 @@
+// Dependencies
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Actions
 import { fetchMetadataSearchResults, fetchArticleSearchResults } from '../../actions/SearchResultActions';
 import { updateSelectedSearchResultsType } from '../../actions/SelectedSearchResultsTypeActions';
-import classNames from 'classnames/bind';
+import { getResource } from '../../actions/ResourceActions'
+
+// Components
 import MetadataSearchResult from './SearchResults/MetadataSearchResult'
 import ArticleSearchResult from './SearchResults/ArticleSearchResult'
 import { ErrorBoundary } from '../../components/ErrorBoundary'
 import FacetFilter from './FacetFilter'
 
+// Stylesheets
 import style from './SearchResults.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 class SearchResults extends Component {
@@ -21,12 +28,12 @@ class SearchResults extends Component {
             tabs: [
                 {
                     id: 'metadata',
-                    name: 'Kartkatalog',
+                    name: this.props.getResource('MapCatalog', 'Kartkatalogen'),
                     counterProperty: 'NumFound'
                 },
                 {
                     id: 'articles',
-                    name: 'Artikler',
+                    name: this.props.getResource('Articles', 'Artikler'),
                     counterProperty: 'NumFound'
                 }
             ]
@@ -94,7 +101,11 @@ class SearchResults extends Component {
         let listItems = this.props.searchResults.metadata && this.props.searchResults.metadata.Results ? this.props.searchResults.metadata.Results : null;
         if (listItems) {
             let listItemElements = listItems.map((searchResult, i) => {
-                return <ErrorBoundary key={i}><MetadataSearchResult searchResult={searchResult} visibleFields={['DownloadButton', 'MapButton', 'ApplicationButton']} key={i} /></ErrorBoundary>;
+                return (
+                    <ErrorBoundary key={i}>
+                        <MetadataSearchResult searchResult={searchResult} visibleFields={['DownloadButton', 'MapButton', 'ApplicationButton']} key={i} />
+                    </ErrorBoundary>
+                );
             });
             return React.createElement('div', { className: style.list, key: "searchResult" }, listItemElements);
         } else {
@@ -106,67 +117,99 @@ class SearchResults extends Component {
         let listItems = this.props.searchResults.articles && this.props.searchResults.articles.Results ? this.props.searchResults.articles.Results : null;
         if (listItems) {
             let listItemElements = listItems.map((searchResult, i) => {
-                return <ErrorBoundary key={i}><ArticleSearchResult searchResult={searchResult} key={i} /></ErrorBoundary>;
+                return (
+                    <ErrorBoundary key={i}>
+                        <ArticleSearchResult searchResult={searchResult} key={i} />
+                    </ErrorBoundary>
+                );
             });
             return React.createElement('div', { className: style.list }, listItemElements);
         } else {
             return "";
         }
     }
-    
-    downloadAsCsvUrl()
-    {
+
+    downloadAsCsvUrl() {
         return localStorage.getItem('urlDownloadCsv');
     }
 
     renderActiveTabContent() {
-        let searchString = "";   
+        let searchString = "";
         if (this.props.selectedSearchResultsType === 'metadata') {
             const moreItemButtonClassNames = classNames({
                 [style.morebtn]: true,
                 hidden: !this.moreItemsAvailable()
             });
 
-                if (this.props.searchString && this.props.searchResults && this.props.searchResults.metadata && this.props.searchResults.metadata.NumFound) {
-                    searchString = 'Søk på "' + this.props.searchString + '" ga ' + this.props.searchResults.metadata.NumFound + ' treff'
-                }
-                return <div className={style.activeContent}>
+            if (this.props.searchString && this.props.searchResults && this.props.searchResults.metadata && this.props.searchResults.metadata.NumFound) {
+                searchString = this.props.searchResults.metadata.NumFound === 1
+                    ? this.props.getResource('SearchResultCountText', 'Søk på {0} ga {1} treff', [this.props.searchString, this.props.searchResults.metadata.NumFound])
+                    : this.props.getResource('SearchResultsCountText', 'Søk på {0} ga {1} treff', [this.props.searchString, this.props.searchResults.metadata.NumFound]);
+            }
+            return (
+                <div className={style.activeContent}>
                     <div className={style.facets}>
-                    <ErrorBoundary key="facetFilter">
-                        <FacetFilter key="facetFilter" />
-                    </ErrorBoundary>
+                        <ErrorBoundary key="facetFilter">
+                            <FacetFilter key="facetFilter" />
+                        </ErrorBoundary>
                     </div>
                     <div className={style.searchResultContainer}>
-                        <span className={searchString !== "" ? style.searchResultInformation : ""}>{searchString} <span className={searchString !== "" ? 'show' : 'hide'}><Link to="/"> Nullstill søk<FontAwesomeIcon title="Nullstill søk" className={style.resetSearchResults} icon={'times'} /></Link></span></span>
+
+                        <span className={searchString !== "" ? style.searchResultInformation : ""}>{searchString}
+                            <span className={searchString !== "" ? 'show' : 'hide'}>
+                                <Link to="/"> {this.props.getResource('ClearSearch', 'Nullstill søk')}
+                                    <FontAwesomeIcon title={this.props.getResource('ClearSearch', 'Nullstill søk')} className={style.resetSearchResults} icon={'times'} />
+                                </Link>
+                            </span>
+                        </span>
                         {this.renderMetadataSearchResults()}
                         <div className={style.downloadcsv}>
-                        <a href={this.downloadAsCsvUrl()}>Lagre listen som csv</a>
+                            <a href={this.downloadAsCsvUrl()}>
+                                {this.props.getResource('SaveResultsAsCSV', 'Lagre listen som CSV')}
+                            </a>
                         </div>
                         <div className={style.morecontainer}>
                             <div className={moreItemButtonClassNames} onClick={() => this.addMoreMetadataToSearchResult()}>
-                                <span>Vis flere</span> <FontAwesomeIcon icon={'angle-down'} key="icon" /></div>
+                                <span>
+                                    {this.props.getResource('ShowMoreResults', 'Vis flere')}
+                                </span>
+                                <FontAwesomeIcon icon={'angle-down'} key="icon" />
+                            </div>
                         </div>
                     </div>
-                </div>;
+                </div>
+            )
 
         } else if (this.props.selectedSearchResultsType === 'articles') {
-
-                if (this.props.searchString) {
-                    searchString = 'Søk på"' + this.props.searchString + '" ga ' + this.props.searchResults.articles.NumFound + ' treff'
-                }
-                const moreItemButtonClassNames = classNames({
-                    [style.morebtn]: true,
-                    hidden: !this.moreArticlesAvailable()
-                });
-                return <div className={style.searchResultContainer}>
-                    <span className={searchString !== "" ? style.searchResultInformation : ""}>{searchString} <span className={searchString !== "" ? 'show' : 'hide'}><Link to="/"> Nullstill søk   <FontAwesomeIcon title="Nullstill søk" className={style.resetSearchResults} icon={'times'} /></Link></span></span>
+            if (this.props.searchString) {
+                searchString = this.props.searchResults.articles.NumFound === 1
+                    ? this.props.getResource('SearchResultCountText', 'Søk på {0} ga {1} treff', [this.props.searchString, this.props.searchResults.articles.NumFound])
+                    : this.props.getResource('SearchResultsCountText', 'Søk på {0} ga {1} treff', [this.props.searchString, this.props.searchResults.articles.NumFound]);
+            }
+            const moreItemButtonClassNames = classNames({
+                [style.morebtn]: true,
+                hidden: !this.moreArticlesAvailable()
+            });
+            return (
+                <div className={style.searchResultContainer}>
+                    <span className={searchString !== "" ? style.searchResultInformation : ""}>
+                        {searchString}
+                        <span className={searchString !== "" ? 'show' : 'hide'}>
+                            <Link to="/">
+                                {this.props.getResource('ClearSearch', 'Nullstill søk')}
+                                <FontAwesomeIcon title={this.props.getResource('ClearSearch', 'Nullstill søk')} className={style.resetSearchResults} icon={'times'} />
+                            </Link>
+                        </span>
+                    </span>
                     {this.renderArticleSearchResults()}
                     <div className={style.morecontainer}>
                         <div className={moreItemButtonClassNames} onClick={() => this.addMoreArticlesToSearchResult()}>
-                            <span>Vis flere</span> <FontAwesomeIcon icon={'angle-down'} key="icon" /></div>
+                            <span>{this.props.getResource('ShowMoreResults', 'Vis flere')}</span>
+                            <FontAwesomeIcon icon={'angle-down'} key="icon" />
+                        </div>
                     </div>
-                </div>;
-
+                </div>
+            );
         } else {
             return (
                 ""
@@ -195,14 +238,15 @@ const mapStateToProps = state => ({
     searchResults: state.searchResults,
     selectedSearchResultsType: state.selectedSearchResultsType,
     selectedFacets: state.selectedFacets,
-    searchString: state.searchString
-
+    searchString: state.searchString,
+    resources: state.resources
 });
 
 const mapDispatchToProps = {
     fetchMetadataSearchResults,
     fetchArticleSearchResults,
-    updateSelectedSearchResultsType
+    updateSelectedSearchResultsType,
+    getResource
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
