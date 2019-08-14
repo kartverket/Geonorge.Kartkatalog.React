d2f16c58f0557ebc6bde60e9627751b35c46607b
Benjamin Dehli <benjamin@arkitektum.no>
2019-08-14 12:46:46 +0200

    Fix logic for selected facets component

--- src/components/partials/SelectedFacets.js
+++ src/components/partials/SelectedFacets.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateAvailableFacets, updateSelectedFacetsFromUrl } from '../../actions/FacetFilterActions'
 import { getResource } from '../../actions/ResourceActions'
-import {ErrorBoundary} from '../ErrorBoundary'
-import {getQueryStringFromFacets} from '../../helpers/FacetFilterHelpers'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Link} from "react-router-dom";
-import PropTypes from 'prop-types';
+import { getQueryStringFromFacets } from '../../helpers/FacetFilterHelpers'
+import { Link } from "react-router-dom";
 
 class SelectedFacets extends Component {
 
@@ -16,42 +13,42 @@ class SelectedFacets extends Component {
             return Object.keys(this.props.selectedFacets).map(facetTypeName => {
                 const facetType = this.props.selectedFacets[facetTypeName];
                 return (
-                   this.renderSelectedFacetType(facetType)
+                    this.renderSelectedFacetType(facetType)
                 )
             });
         }
     }
 
-    renderSelectedFacetType(facetType, facet = null) {
-        
-        if(facetType.facets && Object.keys(facetType.facets).length) {
-            return Object.keys(facetType.facets).map(facetName => {                
-                facet = facet ? facet : facetType.facets[facetName];
-                const hasChildren = facet.facets && Object.keys(facet.facets).length;        
-                let childFacetElements = []                      
-                if(hasChildren) {                    
-                   const childFacets = facet.facets;
+    renderSelectedFacetType(facetType, childFacet) {
+
+        if (facetType.facets && Object.keys(facetType.facets).length) {
+            return Object.keys(facetType.facets).map(facetTypeKey => {
+                const facet = childFacet ? childFacet : facetType.facets[facetTypeKey];
+                const hasChildren = facet.facets && Object.keys(facet.facets).length;
+                let childFacetElements = []
+                if (hasChildren) {
+                    const childFacets = facet.facets;
                     Object.keys(childFacets).forEach(childFacetKey => {
                         const childFacet = childFacets[childFacetKey]
                         childFacetElements.push(this.renderSelectedFacetType(facetType, childFacet))
-                    });                    
-                }              
-                return (   
-                    <div key={facet.Name}> 
+                    });
+                }
+                return (
+                    <div key={facet.Name}>
                         {childFacetElements}
-                        <span>{facet.NameTranslated} <Link to={this.getRemoveFacetQueryString(facet, facetType.Name)}> X</Link></span>                    
+                        <span>{facet.NameTranslated} <Link to={this.getRemoveFacetQueryString(facet, facetType.Name)}> X</Link></span>
                     </div>
-                )                
+                );
             })
         }
-    }  
+    }
     getRemoveFacetQueryString(facet, facetField) {
         return getQueryStringFromFacets(
             this.props.selectedFacets,
             this.props.searchString,
             {
                 facetToRemove: {
-                    facetField, 
+                    facetField,
                     facet
                 }
             });
@@ -59,16 +56,17 @@ class SelectedFacets extends Component {
 
     render() {
         return (
-        <div>{this.renderSelectedFacetsList()}</div>
-        )}
+            <div>{this.renderSelectedFacetsList()}</div>
+        )
+    }
 }
 
-const mapStateToProps = state => ({    
-    selectedFacets: state.selectedFacets   
+const mapStateToProps = state => ({
+    selectedFacets: state.selectedFacets
 });
 const mapDispatchToProps = {
     updateSelectedFacetsFromUrl,
-    updateAvailableFacets,        
+    updateAvailableFacets,
     getResource
 };
 export default connect(mapStateToProps, mapDispatchToProps)(SelectedFacets);
