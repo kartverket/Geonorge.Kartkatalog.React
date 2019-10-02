8f4f0ef198ec5e22cfe55c23fe91fa178ee481b3
Benjamin Dehli <benjamin@arkitektum.no>
2019-10-02 14:54:48 +0200

    Adjust action for public enviroment variables

--- src/actions/EnvironmentActions.js
+++ src/actions/EnvironmentActions.js
@@ -3,9 +3,7 @@ import { GET_ENVIRONMENT } from './types';
 export const getEnvironment = () => dispatch => {
     const environment = process && process.env
     ? {
-      buildNumber: process.env.REACT_APP_BUILD_NUMBER ? process.env.REACT_APP_BUILD_NUMBER : null,
-      majorMinorVersionMaster: process.env.REACT_APP_MAJOR_MINOR_VERSION_MASTER ? process.env.REACT_APP_MAJOR_MINOR_VERSION_MASTER : null,
-      majorMinorVersionDevelopment: process.env.REACT_APP_MAJOR_MINOR_VERSION_DEVELOPMENT ? process.env.REACT_APP_MAJOR_MINOR_VERSION_DEVELOPMENT : null
+      buildNumber: process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : null,
     } : {};
     dispatch({
         type: GET_ENVIRONMENT,
