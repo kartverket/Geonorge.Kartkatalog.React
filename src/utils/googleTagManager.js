e8f08351b95bf0b196c71e8209369abede5c4e3e
Benjamin Dehli <superelg@gmail.com>
2019-12-03 16:26:25 +0100

    Add null check for Tag Manager

--- src/utils/googleTagManager.js
+++ src/utils/googleTagManager.js
@@ -1,17 +1,28 @@
 import {Types, getDataLayer} from '../reducers/TagManagerReducer';
 
-const DATA_LAYER_CONTAINER = 'dataLayer';
-const _dataLayer = window[DATA_LAYER_CONTAINER] || [];
-const _gtm = window.google_tag_manager['GTM-MR2X5P'];
+
 
 export default({getState}) => {
+  const DATA_LAYER_CONTAINER = 'dataLayer';
+  const _dataLayer = window[DATA_LAYER_CONTAINER] || [];
+
+  // TODO: Finne bedre løsning for denne
+  const _gtm = window.google_tag_manager && window.google_tag_manager['GTM-MR2X5P'] ? window.google_tag_manager['GTM-MR2X5P'] : () => {
+    setTimeout(function(){
+      return window.google_tag_manager && window.google_tag_manager['GTM-MR2X5P'] ? window.google_tag_manager['GTM-MR2X5P'] : null
+    }, 300);
+  };
+
+  if (!_gtm){console.log("NO GTM")}
   return(next) => (action) => {
     const returnValue = next(action);
 
-    if (action.type === Types.PUSH_TO_DATALAYER) {
-      _dataLayer.push(getDataLayer(getState()));
-    } else if (action.type === Types.RESET_DATALAYER) {
-      _gtm[DATA_LAYER_CONTAINER].reset();
+    if (_gtm) {
+      if (action.type === Types.PUSH_TO_DATALAYER) {
+        _dataLayer.push(getDataLayer(getState()));
+      } else if (action.type === Types.RESET_DATALAYER) {
+        _gtm[DATA_LAYER_CONTAINER].reset();
+      }
     }
 
     return returnValue;
