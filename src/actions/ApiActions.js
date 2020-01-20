951d5493574b63f1ea17d7c4fc065f282eecfece
Benjamin Dehli <superelg@gmail.com>
2020-01-20 13:11:55 +0100

    Add actions for API requests

---
+++ src/actions/ApiActions.js
@@ -0,0 +1,14 @@
+export const getApiData = (url) => (dispatch, getState) => {
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+  return fetch(url, fetchOptions).then(res => res.json()).then(
+    apiData => {
+      return apiData
+    }
+  )
+}
