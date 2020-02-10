7d6b155973b5f21d68d9eaa8ad3ed5598f4b2a73
Benjamin Dehli <superelg@gmail.com>
2020-02-10 12:56:30 +0100

    Improve helper for URL slugs

--- src/helpers/UrlHelpers.js
+++ src/helpers/UrlHelpers.js
@@ -55,9 +55,41 @@ export const convertPath = (urlPathname) => {
     return isOldUrlPathnameType ? urlPathname.replace(/metadata\/[a-z0-9-]*/is, 'metadata') : urlPathname;
 }
 
-export const convertTextToUrlSlug = text => {
-  let convertedText = text.replace(/ /g, '-'); // Replace all whiteSpaces with dashes
-  return convertedText.toLowerCase(); // Use lower casing
+const replaceAndAddSpace = (string, replace, replaceWith) => {
+  string = string.replace(new RegExp(`([^\s])([${replace}])([^\s])`, 'ig'), `\$1 ${replaceWith} \$3`); // Character right before and after
+  string = string.replace(new RegExp(`([^\s])([${replace}])`, 'ig'), `\$1 ${replaceWith}`); // Character right before
+  string = string.replace(new RegExp(`([${replace}])([^\s])`, 'ig'), `${replaceWith} \$2`); // Character right after
+  string = string.replace(new RegExp(`[${replace}]`, 'ig'), replaceWith); // No character right before or after
+
+  return string;
+}
+
+export const convertTextToUrlSlug = string => {
+  if (string) {
+    // To lower case
+    string = string.toLowerCase();
+
+    // Character replace
+    string = replaceAndAddSpace(string, "&", "and");
+    string = replaceAndAddSpace(string, "+", "plus");
+    string = string.replace("æ", "ae");
+    string = string.replace("ø", "oe");
+    string = string.replace("å", "aa");
+
+    // Whitespace replace
+    string = string.replace(" - ", "-");
+    string = string.replace(/[\s]+/g, "-");
+
+    // Unwated character replace
+    string = string.replace(/[^a-z0-9-]+/ig, "");
+
+    // Remove any character before first and after last A-Z or 0-9
+    string = string.replace(/^[^A-Z0-9]*|[^a-z0-9]*$/ig, "");
+
+    return string;
+  } else {
+    return '';
+  }
 }
 
 export const convertUrlSlugToText = urlSlug => {
