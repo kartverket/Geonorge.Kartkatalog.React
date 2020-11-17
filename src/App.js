e8724dfaa895e4e63659f155c69b894aba11d9ff
Benjamin Dehli <superelg@gmail.com>
2020-11-17 14:06:36 +0100

    Add route for rendered fallback and 404 page

--- src/App.js
+++ src/App.js
@@ -64,7 +64,9 @@ class App extends Component {
                   <Route exact path="/metadata/:title/:uuid" render={(props) => (<Metadata {...props}/>)}/>
                   <Route exact path="/metadata/:uuid" render={(props) => (<Metadata {...props}/>)}/>
                   <Route exact path="/:category" render={(props) => (<Home {...props}/>)}/>
-                  <Route render={() => (<NotFound/>)}/>
+                  <Route key={"/shell.html"} path="/shell.html" component={() => null} />
+                  <Route key={"/404.html"} component={NotFound} />
+                  <Route render={() => (<NotFound />)}/>
                 </Switch>
               </div>
               <footer className={style.footer}>
