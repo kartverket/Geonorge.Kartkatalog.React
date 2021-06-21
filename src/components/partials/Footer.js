ef07692f931c4ca3c5465ff7571fdc32e56422f3
Benjamin Dehli <superelg@gmail.com>
2021-06-21 15:02:09 +0200

    Use footer from Geonorge WebComponents

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -2,74 +2,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Geonorge Webcomponents
+import { GeonorgeFooter } from '@kartverket/geonorge-web-components/GeonorgeFooter';
+
 // Actions
 import { getResource } from 'actions/ResourceActions'
-import { getGeonorgeLogo } from 'actions/ImageActions';
 
 // Stylesheets
 import style from 'components/partials/Footer.module.scss';
 
-// Assets
-import kartverketLogo from 'images/kartverket.png';
-
 
 export class Footer extends Component {
-
-	getBuildNumber() {
-		return this.props.environment && this.props.environment.buildNumber
-		? (
-				<p>
-					<small>{this.props.getResource('Version', 'Versjon')} {this.props.environment.buildNumber}</small>
-				</p>
-		)
-		: '';
-	}
-
 	render() {
-		const isMapRoute = this.props.router && this.props.router.location && this.props.router.location.pathname === "/kart";
+		const isMapRoute = this.props.router?.location?.pathname === "/kart";
 		return !isMapRoute ? (
-			<div className={style.container}>
-				<div className={style.logo}>
-					<img src={this.props.getGeonorgeLogo()} alt="Geonorge logo" title="Logo for Geonorge" />
-					{this.getBuildNumber()}
+			<React.Fragment>
+				<geonorge-footer language={this.props.selectedLanguage} environment={this.props.environment?.environment} />
+				<div className={style.buildNumber}>
+					{
+						this.props.environment?.buildNumber
+							? (
+								<p>
+									<small>{this.props.getResource('Version', 'Versjon')} {this.props.environment.buildNumber}</small>
+								</p>
+							)
+							: ''
+					}
 				</div>
-				<div className={style.about}>
-					<h2 className={style.heading}>{this.props.getResource('AboutTheSite', 'Om nettstedet')}</h2>
-					<p>
-						<a href="https://www.geonorge.no/aktuelt/om-geonorge/">
-							{this.props.getResource('AboutGeonorge', 'Om Geonorge')}
-						</a>
-					</p>
-					<p>
-						<a href="https://www.geonorge.no/aktuelt/Nyheter/annet/personvern-og-bruk-av-cookies/#_ga=2.93024157.1065069024.1548754965-263923791.1536837570">
-							{this.props.getResource('PrivacyAndUseOfCookies', 'Personvern og bruk av cookies')}
-						</a>
-					</p>
-				</div>
-				<div className={style.contact}>
-					<h2 className={style.heading}>{this.props.getResource('ContactUs', 'Kontakt')}</h2>
-					<p>{this.props.getResource('Telephone', 'Telefon')}: 32 11 80 00</p>
-					<p>post@norgedigitalt.no</p>
-					<p>{this.props.getResource('OrgNo', 'Org. nr.')}: 971 040 238</p>
-					<hr />
-					{this.props.getResource('ASolutionBy', 'En løsning fra')}
-					<div className={style.logo}><img src={kartverketLogo} alt="Kartverket logo" title="Logo for Kartverket" /></div>
-				</div>
-			</div>
+			</React.Fragment>
 		) : ''
 	}
 }
 
 
 const mapStateToProps = state => ({
-    router: state.router,
+	router: state.router,
 	resources: state.resources,
-	environment: state.environment
+	environment: state.environment,
+	selectedLanguage: state.selectedLanguage
 });
 
 const mapDispatchToProps = {
-	getResource,
-    getGeonorgeLogo
+	getResource
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Footer);
