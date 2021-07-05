99ca407941b5f8ffffe0d615975497320b1202b7
Benjamin Dehli <superelg@gmail.com>
2021-07-05 12:27:09 +0200

    Move version number text into footer component

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -8,27 +8,13 @@ import { GeonorgeFooter } from '@kartverket/geonorge-web-components/GeonorgeFoot
 // Actions
 import { getResource } from 'actions/ResourceActions'
 
-// Stylesheets
-import style from 'components/partials/Footer.module.scss';
-
 
 export class Footer extends Component {
 	render() {
 		const isMapRoute = this.props.router?.location?.pathname === "/kart";
 		return !isMapRoute ? (
 			<React.Fragment>
-				<geonorge-footer language={this.props.selectedLanguage} environment={this.props.environment?.environment} />
-				<div className={style.buildNumber}>
-					{
-						this.props.environment?.buildNumber
-							? (
-								<p>
-									<small>{this.props.getResource('Version', 'Versjon')} {this.props.environment.buildNumber}</small>
-								</p>
-							)
-							: ''
-					}
-				</div>
+				<geonorge-footer language={this.props.selectedLanguage} environment={this.props.environment?.environment} version={this.props.environment.buildNumber} />
 			</React.Fragment>
 		) : ''
 	}
