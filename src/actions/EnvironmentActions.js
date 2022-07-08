3ee33310d1b489bb55e699603ea3501ede9c469a
Benjamin Dehli <superelg@gmail.com>
2022-07-08 10:05:32 +0200

    Remove unused code and components

--- src/actions/EnvironmentActions.js
+++ src/actions/EnvironmentActions.js
@@ -1,13 +1,12 @@
 import { GET_ENVIRONMENT } from 'actions/types';
 
 export const getEnvironment = () => dispatch => {
-    const environment = process && process.env
-    ? {
+    const environment =  {
       buildNumber: process.env.REACT_APP_BUILD_NUMBER || null,
       majorMinorVersionMaster: process.env.REACT_APP_MAJOR_MINOR_VERSION_MASTER || null,
       majorMinorVersionDevelopment: process.env.REACT_APP_MAJOR_MINOR_VERSION_DEVELOPMENT || null,
       environment: process.env.REACT_APP_ENVIRONMENT || null
-    } : {};
+    };
     dispatch({
         type: GET_ENVIRONMENT,
         payload: environment
