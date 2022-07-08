ccde83daa3396d8290a3ebd3e32f34c2309e7b68
Benjamin Dehli <superelg@gmail.com>
2022-07-08 10:12:14 +0200

    Migrate code for breadcrumb

--- src/components/partials/Breadcrumb.js
+++ src/components/partials/Breadcrumb.js
@@ -1,77 +1,62 @@
 // Dependencies
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Actions
-import { getResource } from 'actions/ResourceActions';
+import { getResource } from "actions/ResourceActions";
 
 // Stylesheets
-import style from 'components/partials/Breadcrumb.module.scss';
+import style from "components/partials/Breadcrumb.module.scss";
 
+const Breadcrumb = (props) => {
+    const dispatch = useDispatch();
 
-export class Breadcrumb extends Component {
-
-    renderBreadcrumb() {
+    const renderBreadcrumb = () => {
         return (
             <div className={style.breadCrumb}>
                 <ul vocab="https://schema.org/" typeof="BreadcrumbList">
                     <li property="itemListElement" typeof="ListItem">
-                        <a property="item" typeof="WebPage" href={'https://www.geonorge.no/'}>
-                          <span property="name">Geonorge</span>
+                        <a property="item" typeof="WebPage" href={"https://www.geonorge.no/"}>
+                            <span property="name">Geonorge</span>
                         </a>
-                         <meta property="position" content="1" />
-                        <FontAwesomeIcon title="Til Geonorge" icon={'angle-right'} />
+                        <meta property="position" content="1" />
+                        <FontAwesomeIcon title="Til Geonorge" icon={"angle-right"} />
                     </li>
-                    {this.renderCurrentBreadcrumb()}
+                    {renderCurrentBreadcrumb()}
                 </ul>
             </div>
-        )
-    }
+        );
+    };
 
-    renderCurrentBreadcrumb() {
-        if (this.props.content) {
+    const renderCurrentBreadcrumb = () => {
+        if (props.content) {
             return (
                 <React.Fragment>
                     <li property="itemListElement" typeof="ListItem">
-                      <Link to={'/'} property="item" typeof="WebPage">
-                        <span property="name">{ this.props.getResource('AppPageTitle', 'Kartkatalogen') }</span>
-                      </Link>
-                      <meta property="position" content="2" />
-                      <FontAwesomeIcon title="Tilbake til katalogen" icon={'angle-right'} />
+                        <Link to={"/"} property="item" typeof="WebPage">
+                            <span property="name">{dispatch(getResource("AppPageTitle", "Kartkatalogen"))}</span>
+                        </Link>
+                        <meta property="position" content="2" />
+                        <FontAwesomeIcon title="Tilbake til katalogen" icon={"angle-right"} />
                     </li>
                     <li property="itemListElement" typeof="ListItem">
-                      <span property="name">{this.props.content}</span>
-                      <meta property="position" content="3" />
+                        <span property="name">{props.content}</span>
+                        <meta property="position" content="3" />
                     </li>
                 </React.Fragment>
-            )
+            );
         }
-        return (
-          <li> { this.props.getResource('AppPageTitle', 'Kartkatalogen') } </li>
-        )
-    }
+        return <li> {dispatch(getResource("AppPageTitle", "Kartkatalogen"))} </li>;
+    };
 
-    render() {
-        return this.renderBreadcrumb()
-    }
-}
-
-
+    return renderBreadcrumb();
+};
 
 Breadcrumb.propTypes = {
-    content: PropTypes.string,
+    content: PropTypes.string
 };
 
-const mapStateToProps = state => ({
-    resources: state.resources,
-    searchString: state.searchString
-});
-
-const mapDispatchToProps = {
-    getResource
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Breadcrumb);
+export default Breadcrumb;
