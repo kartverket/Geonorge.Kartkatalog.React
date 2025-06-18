d701d7f8145c8af51242cd9f83c6776d4e33cae2
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2025-06-18 14:01:32 +0200

    La til GnShortcutButton

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,10 +7,6 @@ import { ErrorBoundary } from "./ErrorBoundary";
 import Footer from "./partials/Footer";
 import MainNavigationContainer from "./partials/MainNavigationContainer";
 
-// Geonorge WebComponents
-// eslint-disable-next-line no-unused-vars
-import { GnShortcutButton } from "@kartverket/geonorge-web-components";
-
 // Stylesheets
 import style from "./Layout.module.scss";
 import { useSelector } from "react-redux";
@@ -18,35 +14,17 @@ import { useSelector } from "react-redux";
 const Layout = (props) => {
     // Redux store
     const oidc = useSelector((state) => state.oidc);
-    const environment = useSelector((state) => state.environment);
-    const selectedLanguage = useSelector((state) => state.selectedLanguage);
-
     const layoutLoaderData = useLoaderData();
 
     // Refs
     const userRef = useRef(null);
 
-    function getToken() {
-        const token = oidc?.user?.access_token;
-        return token?.length ? token : null;
-    }
-
     useEffect(() => {
         userRef.current = oidc?.user;
     }, [oidc]);
 
-    useEffect(() => {
-        const isLoggedIn = !!oidc?.user?.access_token?.length;
-        if (isLoggedIn) {
-            GnShortcutButton.setup("gn-shortcut-button", {
-                getAuthToken: getToken
-            });
-        }
-    }, [getToken, oidc]);
-
     return (
         <ErrorBoundary>
-            <gn-shortcut-button language={selectedLanguage} environment={environment?.environment}></gn-shortcut-button>
             <MainNavigationContainer layoutLoaderData={layoutLoaderData} userManager={props.userManager} />
             <content-container fullwidth class={style.test} id="main-content">
                 <Outlet />
