0c82d3051abfdd06e44b508438e33fc473a9fa7a
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2026-03-02 14:21:24 +0100

    migrate SCSS from @import to @use/@forward and anable modert api in sass-loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,26 @@
 const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = function override(config, env) {
-    config.plugins.push(new CompressionPlugin())
+    config.plugins.push(new CompressionPlugin());
+
+    // Use the modern Sass JS API to suppress legacy API deprecation warnings
+    config.module.rules.forEach(rule => {
+        const oneOf = rule.oneOf;
+        if (!oneOf) return;
+        oneOf.forEach(r => {
+            const use = r.use;
+            if (!use) return;
+            use.forEach(loader => {
+                if (
+                    loader.loader &&
+                    loader.loader.includes('sass-loader') &&
+                    loader.options
+                ) {
+                    loader.options.api = 'modern';
+                }
+            });
+        });
+    });
+
     return config;
 };
