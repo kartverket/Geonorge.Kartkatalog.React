8f353040940f771aff28959c56a45e03c7183ff2
Bjoost <bjorn-erik.ostby@kartverket.no>
2026-03-10 10:20:00 +0100

    1. Oppdatert nvmrc til 24 ( 24.14.0) siste lts. 2. fikset env import i Serviceworker.js bruker nå import.meta.env. 3. Flyttet innholdet fra index.jsx til main.jsx, istedenfor at main.jsx bare er en wrapper. 4. Fikset testing, Enzyme react testing var påbegynt og ikke fullført, skapte stopp i testing.

--- src/App.test.disabled.jsx
+++ src/App.test.disabled.jsx
@@ -17,10 +17,15 @@ function setup() {
   }
 }*/
 
-describe('App', () => {
+/*describe('App', () => {
     it('should render self and subcomponents', () => {
       //const { wrapper } = setup()
       //expect(wrapper).toMatchSnapshot();
       expect(1).toBe(1);
     })
 })
+
+*/
+
+//Kommenterte ut resten også, den gjør ingenting egentlig. 
+//Endret navn slik at den også ikke kjøres i test. 
