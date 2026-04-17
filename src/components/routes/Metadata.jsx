7780742d79ccd20978acfbe72a00d34c99046c0f
Dag Olav Dahle <dagolav@arkitektum.no>
2026-04-17 11:53:34 +0200

    Bugfix null check

--- src/components/routes/Metadata.jsx
+++ src/components/routes/Metadata.jsx
@@ -426,7 +426,7 @@ const Metadata = () => {
 
     const renderOperations = () => {
 
-        const isEmptyOperations = metadata?.Operations.every(op => Object.keys(op).length === 0);
+        const isEmptyOperations = metadata?.Operations?.every(op => Object.keys(op).length === 0);
 
         const operationsList =
             metadata?.Operations?.length && !isEmptyOperations &&
