96940713ec67c967028788af75b3c8390ee7b549
ingebjrgb <ingebjrgb@gmail.com>
2026-09-02 08:39:17 +0200

    legger til måling på betaklikk

--- src/components/partials/BetaBanner.jsx
+++ src/components/partials/BetaBanner.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { HangerIcon } from "@navikt/aksel-icons";
+import { usePostHog } from "posthog-js/react";
 import style from "./BetaBanner.module.scss";
 
 const BetaBanner = ({ uuid }) => {
     const betaUrl = `${window.location.origin}/beta/metadata/${uuid}`;
+    const posthog = usePostHog();
+
+    const handleBetaLinkClick = () => {
+        posthog?.capture("beta_banner_link_click", {
+            uuid,
+            betaUrl
+        });
+    };
 
     return (
         <div className={style.banner}>
             <HangerIcon aria-hidden className={style.icon} />
             <div className={style.textWrapper}>
                 <p className={style.line1}>Vil du teste de nye produktsidene i ny drakt?</p>
-                <p className={style.line2}>Vi jobber med å oppdatere produktsidene og vil gjerne ha tilbakemeldinger. <a href={betaUrl}>Gå til beta-versjonen</a></p>
+                <p className={style.line2}>Vi jobber med å oppdatere produktsidene og vil gjerne ha tilbakemeldinger. <a href={betaUrl} onClick={handleBetaLinkClick}>Gå til beta-versjonen</a></p>
             </div>
         </div>
     );
